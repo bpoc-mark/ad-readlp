7e709b9c3c78f5b49455eff2e74d46a8e9489046
gregarsua <arsuagreg@gmail.com>
2021-12-14 18:01:48 +0800

    Add: add function on footer

--- src/assets/js/all.js
+++ src/assets/js/all.js
@@ -5,51 +5,34 @@
 
 $(document).ready(function () {
 	//ACCORDION
-	$(".accordion__header-cont").on("click", function () {
+	$(".accordion").on("click", function () {
 		if ($(this).hasClass("active")) {
-			$(this).find("p").text("close");
+			$(this).find("img").css({
+				transform: "rotate(-135deg)",
+				transition: "all 300ms",
+			});
+			$(this).next(".accordion_content").slideDown();
+			$(this).removeClass("active");
+		} else {
 			$(this).find("img").css({
 				transform: "rotate(0deg)",
 				transition: "all 300ms",
 			});
-			$(this).next(".accordion__content").slideDown();
-			$(this).removeClass("active");
-		} else {
-			$(this).find("p").text("open");
-			$(this).find("img").css({
-				transform: "rotate(180deg)",
-				transition: "all 300ms",
-			});
-			$(this).next(".accordion__content").slideUp();
+			$(this).next(".accordion_content").slideUp();
 			$(this).addClass("active");
 		}
 	});
 
 	//NAVBAR TOGGLE
-	$(".hamburger").on("click", function () {
-		$(this).find("span").toggleClass("toggled");
-		$(this).parent(".nav").find(".menu").toggleClass("active");
-		$("#overlay").toggleClass("active");
-	});
-	$("#overlay").on("click", function () {
-		$(".hamburger span").toggleClass("toggled");
-		$(".nav .menu").toggleClass("active");
-		$("#overlay").toggleClass("active");
-	});
+	// $(".hamburger").on("click", function () {
+	// 	$(this).find("span").toggleClass("toggled");
+	// 	$(this).parent(".nav").find(".menu").toggleClass("active");
+	// 	$("#overlay").toggleClass("active");
+	// });
+	// $("#overlay").on("click", function () {
+	// 	$(".hamburger span").toggleClass("toggled");
+	// 	$(".nav .menu").toggleClass("active");
+	// 	$("#overlay").toggleClass("active");
+	// });
 	//END NAVBAR TOGGLE
-
-	//POPOUT ON SCROLLED
-	$(window).on("scroll", function () {
-		$(".setAnime").each(function () {
-			var viewPos = $(this).offset().top,
-				viewScrollTop = $(window).scrollTop(),
-				viewHeight = $(window).height();
-			if (viewScrollTop > viewPos - viewHeight + viewHeight / 4) {
-				$(this).addClass("animated");
-			} else {
-				$(this).removeClass("animated");
-			}
-		});
-	});
-	//END POPOUT
 });
