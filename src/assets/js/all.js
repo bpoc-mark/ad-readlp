e78e51ccdf33a2e7f68945d38208a15ecb2d3abf
gregarsua <arsuagreg@gmail.com>
2021-12-15 10:55:37 +0800

    Change: fix footer padding, icon on drop down menu, add back to top button

--- src/assets/js/all.js
+++ src/assets/js/all.js
@@ -1,38 +1,24 @@
-// ADD OPTGROUP ON SELECT
-// $(function () {
-// 	$("select").append('<optgroup label=""></optgroup>');
-// });
-
+//MENU FOOTER
 $(document).ready(function () {
-	//ACCORDION
 	$(".accordion").on("click", function () {
 		if ($(this).hasClass("active")) {
-			$(this).find("img").css({
-				transform: "rotate(-135deg)",
-				transition: "all 300ms",
-			});
+			$(this)
+				.find("img")
+				.attr(
+					"src",
+					"http://localhost:3000/wp-content/themes/ad-readlp/release/image/minus_sign.png"
+				);
 			$(this).next(".accordion_content").slideDown();
 			$(this).removeClass("active");
 		} else {
-			$(this).find("img").css({
-				transform: "rotate(0deg)",
-				transition: "all 300ms",
-			});
+			$(this)
+				.find("img")
+				.attr(
+					"src",
+					"http://localhost:3000/wp-content/themes/ad-readlp/release/image/plus_sign.png"
+				);
 			$(this).next(".accordion_content").slideUp();
 			$(this).addClass("active");
 		}
 	});
-
-	//NAVBAR TOGGLE
-	// $(".hamburger").on("click", function () {
-	// 	$(this).find("span").toggleClass("toggled");
-	// 	$(this).parent(".nav").find(".menu").toggleClass("active");
-	// 	$("#overlay").toggleClass("active");
-	// });
-	// $("#overlay").on("click", function () {
-	// 	$(".hamburger span").toggleClass("toggled");
-	// 	$(".nav .menu").toggleClass("active");
-	// 	$("#overlay").toggleClass("active");
-	// });
-	//END NAVBAR TOGGLE
 });
