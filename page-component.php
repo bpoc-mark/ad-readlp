d24b79c4f971fc0b36e9303b138234bfaaaca8a3
bpoc-mark <markariel.maata@gmail.com>
2021-12-24 17:38:26 +0800

    added page business pc and sp view / lacking page header / update card component c-card01, c-card02, c-card03

--- page-component.php
+++ page-component.php
@@ -6,65 +6,68 @@ get_header();?>
     <div class="page_component">
         <div class="l-wrap">
             <div class="c-card01">
-                <div class="c-card01--col3">
-                    <div class="c-card01--col3__img">
-                        <img src="<?php echo get_template_directory_uri();?>/release/image/pages/component/c-card01.jpg" alt="">
-                        <span class="c-card01--col3__num">01</span>
+                <h3 class="c-card01--ttl">ご依頼から実施・改善までの主な流れ</h3>
+                <div class="c-card">
+                    <div class="c-card--col3">
+                        <div class="c-card--col3__img">
+                            <img src="<?php echo get_template_directory_uri();?>/release/image/pages/component/c-card01.jpg" alt="">
+                            <span class="c-card--col3__num">01</span>
+                        </div>
+                        <h3 class="c-card--col3__title">お電話かメールにて、お気軽にお電話ください</h3>
+                        <p class="c-card--col3__desc">些細な質問でも構いません。ご相談はお気軽に。まずはお電話かメールでお問い合わせください。すぐに担当者から折り返しご連絡いたしま</p>
                     </div>
-                    <h3 class="c-card01--col3__title">お電話かメールにて、お気軽にお電話ください</h3>
-                    <p class="c-card01--col3__desc">些細な質問でも構いません。ご相談はお気軽に。まずはお電話かメールでお問い合わせください。すぐに担当者から折り返しご連絡いたしま</p>
-                </div>
-                <div class="c-card01--col3">
-                    <div class="c-card01--col3__img">
-                        <img src="<?php echo get_template_directory_uri();?>/release/image/pages/component/c-card01.jpg" alt="">
-                        <span class="c-card01--col3__num">01</span>
+                    <div class="c-card--col3">
+                        <div class="c-card--col3__img">
+                            <img src="<?php echo get_template_directory_uri();?>/release/image/pages/component/c-card01.jpg" alt="">
+                            <span class="c-card--col3__num">01</span>
+                        </div>
+                        <h3 class="c-card--col3__title">お電話かメールにて、お気軽にお電話ください</h3>
+                        <p class="c-card--col3__desc">些細な質問でも構いません。ご相談はお気軽に。まずはお電話かメールでお問い合わせください。すぐに担当者から折り返しご連絡いたしま</p>
                     </div>
-                    <h3 class="c-card01--col3__title">お電話かメールにて、お気軽にお電話ください</h3>
-                    <p class="c-card01--col3__desc">些細な質問でも構いません。ご相談はお気軽に。まずはお電話かメールでお問い合わせください。すぐに担当者から折り返しご連絡いたしま</p>
-                </div>
-                <div class="c-card01--col3">
-                    <div class="c-card01--col3__img">
-                        <img src="<?php echo get_template_directory_uri();?>/release/image/pages/component/c-card01.jpg" alt="">
-                        <span class="c-card01--col3__num">01</span>
+                    <div class="c-card--col3">
+                        <div class="c-card--col3__img">
+                            <img src="<?php echo get_template_directory_uri();?>/release/image/pages/component/c-card01.jpg" alt="">
+                            <span class="c-card--col3__num">01</span>
+                        </div>
+                        <h3 class="c-card--col3__title">お電話かメールにて、お気軽にお電話ください</h3>
+                        <p class="c-card--col3__desc">些細な質問でも構いません。ご相談はお気軽に。まずはお電話かメールでお問い合わせください。すぐに担当者から折り返しご連絡いたしま</p>
                     </div>
-                    <h3 class="c-card01--col3__title">お電話かメールにて、お気軽にお電話ください</h3>
-                    <p class="c-card01--col3__desc">些細な質問でも構いません。ご相談はお気軽に。まずはお電話かメールでお問い合わせください。すぐに担当者から折り返しご連絡いたしま</p>
-                </div>
-                <div class="c-card01--col3">
-                    <div class="c-card01--col3__img">
-                        <img src="<?php echo get_template_directory_uri();?>/release/image/pages/component/c-card01.jpg" alt="">
-                        <span class="c-card01--col3__num">01</span>
+                    <div class="c-card--col3">
+                        <div class="c-card--col3__img">
+                            <img src="<?php echo get_template_directory_uri();?>/release/image/pages/component/c-card01.jpg" alt="">
+                            <span class="c-card--col3__num">01</span>
+                        </div>
+                        <h3 class="c-card--col3__title">お電話かメールにて、お気軽にお電話ください</h3>
+                        <p class="c-card--col3__desc">些細な質問でも構いません。ご相談はお気軽に。まずはお電話かメールでお問い合わせください。すぐに担当者から折り返しご連絡いたしま</p>
                     </div>
-                    <h3 class="c-card01--col3__title">お電話かメールにて、お気軽にお電話ください</h3>
-                    <p class="c-card01--col3__desc">些細な質問でも構いません。ご相談はお気軽に。まずはお電話かメールでお問い合わせください。すぐに担当者から折り返しご連絡いたしま</p>
-                </div>
-                <div class="c-card01--col3">
-                    <div class="c-card01--col3__img">
-                        <img src="<?php echo get_template_directory_uri();?>/release/image/pages/component/c-card01.jpg" alt="">
-                        <span class="c-card01--col3__num">01</span>
+                    <div class="c-card--col3">
+                        <div class="c-card--col3__img">
+                            <img src="<?php echo get_template_directory_uri();?>/release/image/pages/component/c-card01.jpg" alt="">
+                            <span class="c-card--col3__num">01</span>
+                        </div>
+                        <h3 class="c-card--col3__title">お電話かメールにて、お気軽にお電話ください</h3>
+                        <p class="c-card--col3__desc">些細な質問でも構いません。ご相談はお気軽に。まずはお電話かメールでお問い合わせください。すぐに担当者から折り返しご連絡いたしま</p>
                     </div>
-                    <h3 class="c-card01--col3__title">お電話かメールにて、お気軽にお電話ください</h3>
-                    <p class="c-card01--col3__desc">些細な質問でも構いません。ご相談はお気軽に。まずはお電話かメールでお問い合わせください。すぐに担当者から折り返しご連絡いたしま</p>
-                </div>
-                <div class="c-card01--col3">
-                    <div class="c-card01--col3__img">
-                        <img src="<?php echo get_template_directory_uri();?>/release/image/pages/component/c-card01.jpg" alt="">
-                        <span class="c-card01--col3__num">01</span>
+                    <div class="c-card--col3">
+                        <div class="c-card--col3__img">
+                            <img src="<?php echo get_template_directory_uri();?>/release/image/pages/component/c-card01.jpg" alt="">
+                            <span class="c-card--col3__num">01</span>
+                        </div>
+                        <h3 class="c-card--col3__title">お電話かメールにて、お気軽にお電話ください</h3>
+                        <p class="c-card--col3__desc">些細な質問でも構いません。ご相談はお気軽に。まずはお電話かメールでお問い合わせください。すぐに担当者から折り返しご連絡いたしま</p>
                     </div>
-                    <h3 class="c-card01--col3__title">お電話かメールにて、お気軽にお電話ください</h3>
-                    <p class="c-card01--col3__desc">些細な質問でも構いません。ご相談はお気軽に。まずはお電話かメールでお問い合わせください。すぐに担当者から折り返しご連絡いたしま</p>
                 </div>
             </div>
             <pre>
                 <?= trim(htmlspecialchars('
-                    <div class="c-card01">
-                        <div class="c-card01--col3">
-                            <div class="c-card01--col3__img">
+                    <div class="c-card">
+                        <div class="c-card--col3">
+                            <div class="c-card--col3__img">
                                 <img src="<?php echo get_template_directory_uri();?>/release/image/pages/component/c-card01.jpg" alt="">
-                                <span class="c-card01--col3__num">01</span>
+                                <span class="c-card--col3__num">01</span>
                             </div>
-                            <h3 class="c-card01--col3__title">お電話かメールにて、お気軽にお電話ください</h3>
-                            <p class="c-card01--col3__desc">些細な質問でも構いません。ご相談はお気軽に。まずはお電話かメールでお問い合わせください。すぐに担当者から折り返しご連絡いたしま</p>
+                            <h3 class="c-card--col3__title">お電話かメールにて、お気軽にお電話ください</h3>
+                            <p class="c-card--col3__desc">些細な質問でも構いません。ご相談はお気軽に。まずはお電話かメールでお問い合わせください。すぐに担当者から折り返しご連絡いたしま</p>
                         </div>
                     </div>
                 ')) ?>
@@ -117,22 +120,26 @@ get_header();?>
         <br><br><br><br><br>
         <div class="l-wrap">
             <div class="c-card03">
-                <div class="c-card03--col3">
-                    <span class="c-card03--col3--num">01</span>
-                    <h3 class="c-card03--col3--title">地域・ターゲットに合わせ<br>最適な手法を選定</h3>
-                    <p class="c-card03--col3--desc">デジタルプロモーションと言ってもその手法はさまざま。リードでは、さまざまな媒体・施策から、地域・ターゲットに合わせて最適なものを選定します。これにより低コストでより見込み客に向けてピンポイントでアプローチすることができます。</p>
-                </div>
-                <div class="c-card03--col3">
-                    <span class="c-card03--col3--num">01</span>
-                    <h3 class="c-card03--col3--title">地域・ターゲットに合わせ<br>最適な手法を選定</h3>
-                    <p class="c-card03--col3--desc">デジタルプロモーションと言ってもその手法はさまざま。リードでは、さまざまな媒体・施策から、地域・ターゲットに合わせて最適なものを選定します。これにより低コストでより見込み客に向けてピンポイントでアプローチすることができます。</p>
-                </div>
-                <div class="c-card03--col3">
-                    <span class="c-card03--col3--num">01</span>
-                    <h3 class="c-card03--col3--title">地域・ターゲットに合わせ<br>最適な手法を選定</h3>
-                    <p class="c-card03--col3--desc">デジタルプロモーションと言ってもその手法はさまざま。リードでは、さまざまな媒体・施策から、地域・ターゲットに合わせて最適なものを選定します。これにより低コストでより見込み客に向けてピンポイントでアプローチすることができます。</p>
+                <h3 class="c-card03--ttl">ブランディング戦略</h3>
+                <div class="c-card">
+                    <div class="c-card--col3">
+                        <span class="c-card--col3--num">01</span>
+                        <h4 class="c-card--col3--title">地域・ターゲットに合わせ<br>最適な手法を選定</h4>
+                        <p class="c-card--col3--desc">デジタルプロモーションと言ってもその手法はさまざま。リードでは、さまざまな媒体・施策から、地域・ターゲットに合わせて最適なものを選定します。これにより低コストでより見込み客に向けてピンポイントでアプローチすることができます。</p>
+                    </div>
+                    <div class="c-card--col3">
+                        <span class="c-card--col3--num">01</span>
+                        <h4 class="c-card--col3--title">地域・ターゲットに合わせ<br>最適な手法を選定</h4>
+                        <p class="c-card--col3--desc">デジタルプロモーションと言ってもその手法はさまざま。リードでは、さまざまな媒体・施策から、地域・ターゲットに合わせて最適なものを選定します。これにより低コストでより見込み客に向けてピンポイントでアプローチすることができます。</p>
+                    </div>
+                    <div class="c-card--col3">
+                        <span class="c-card--col3--num">01</span>
+                        <h4 class="c-card--col3--title">地域・ターゲットに合わせ<br>最適な手法を選定</h4>
+                        <p class="c-card--col3--desc">デジタルプロモーションと言ってもその手法はさまざま。リードでは、さまざまな媒体・施策から、地域・ターゲットに合わせて最適なものを選定します。これにより低コストでより見込み客に向けてピンポイントでアプローチすることができます。</p>
+                    </div>
                 </div>
             </div>
+            
             <br><br><br>
             <pre>
                 <?= trim(htmlspecialchars('
