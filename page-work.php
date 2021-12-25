e59bd87465620b5a19086a4af59e2bf61a61db87
bpoc-mark <markariel.maata@gmail.com>
2021-12-25 09:39:57 +0800

    added work page pc and sp view / added footer stamp on component.scss

---
+++ page-work.php
@@ -0,0 +1,126 @@
+<?php
+
+/* Template Name: Work Page */
+
+ get_header();?>
+<div class="page_work">
+    <section class="sect-1">
+        <div class="l-wrap">
+            <div class="work_row">
+                <div class="work_row--item">
+                    <p class="work_row--item__sub">
+                        <span>・ </span>エステ
+                    </p>
+                    <figure>
+                        <img src="<?php echo get_template_directory_uri();?>/release/image/pages/work/img_1.jpg" alt="">
+                    </figure>
+                    <div class="work_row--item__client">
+                        <span>Client</span>
+                        <p>リッツ</p>
+                    </div>
+                    <div class="work_row--item__target">
+                        <span>Target</span>
+                        <p>ターゲット入ります</p>
+                    </div>
+                    <p class="work_row--item__content">
+                    この文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章は100w
+                    </p>
+                </div>
+                <div class="work_row--item">
+                    <p class="work_row--item__sub">
+                        <span>・ </span>業種
+                    </p>
+                    <figure>
+                        <img src="<?php echo get_template_directory_uri();?>/release/image/pages/work/img_1.jpg" alt="">
+                    </figure>
+                    <div class="work_row--item__client">
+                        <span>Client</span>
+                        <p>クライアント名が入ります</p>
+                    </div>
+                    <div class="work_row--item__target">
+                        <span>Target</span>
+                        <p>ターゲット入ります</p>
+                    </div>
+                    <p class="work_row--item__content">
+                    この文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章は100w
+                    </p>
+                </div>
+                <div class="work_row--item">
+                    <p class="work_row--item__sub">
+                        <span>・ </span>エステ
+                    </p>
+                    <figure>
+                        <img src="<?php echo get_template_directory_uri();?>/release/image/pages/work/img_1.jpg" alt="">
+                    </figure>
+                    <div class="work_row--item__client">
+                        <span>Client</span>
+                        <p>リッツ</p>
+                    </div>
+                    <div class="work_row--item__target">
+                        <span>Target</span>
+                        <p>ターゲット入ります</p>
+                    </div>
+                    <p class="work_row--item__content">
+                    この文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章は100w
+                    </p>
+                </div>
+                <div class="work_row--item">
+                    <p class="work_row--item__sub">
+                        <span>・ </span>エステ
+                    </p>
+                    <figure>
+                        <img src="<?php echo get_template_directory_uri();?>/release/image/pages/work/img_1.jpg" alt="">
+                    </figure>
+                    <div class="work_row--item__client">
+                        <span>Client</span>
+                        <p>リッツ</p>
+                    </div>
+                    <div class="work_row--item__target">
+                        <span>Target</span>
+                        <p>ターゲット入ります</p>
+                    </div>
+                    <p class="work_row--item__content">
+                    この文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章は100w
+                    </p>
+                </div>
+            </div>
+            <div class="btn_wrap">
+                <button class="c-buttonRight">
+                    <a href=""><span class="jp_txt">その他の制作実績を見る</span></a>
+                    <span><img src="<?php echo get_template_directory_uri();?>/release/image/read_morebigger.svg"
+                            alt=""></span>
+                </button>
+                <button class="c-buttonRight">
+                    <a href=""><span class="jp_txt">事業領域を見る</span></a>
+                    <span><img src="<?php echo get_template_directory_uri();?>/release/image/read_morebigger.svg"
+                            alt=""></span>
+                </button>
+            </div>
+        </div>
+    </section>
+    <section class="c-stamp">
+        <div class="l-wrap">
+            <div class="c-stamp--wrap">
+                <h3 class="c-stamp--wrap__ttl">Contact</h3>
+                <p class="c-stamp--wrap__desc">
+                ご依頼に関するお問い合せは下記までご連絡ください。<br>
+                <br>
+                <span>株式会社リード</span><br>
+                〒150-0002 東京都渋谷区渋谷1-11-1 COI西青山ビル4F
+                </p>
+                <div class="c-stamp--wrap__row">
+                    <div class="c-stamp--wrap__row--col">
+                        <a href="tel:03-6418-2475">TEL : 03-6418-2475</a>
+                    </div>
+                    <div class="c-stamp--wrap__row--col">
+                        <span>FAX : 03-6418-2476</span>
+                    </div>
+                    <div class="c-stamp--wrap__row--col">
+                        <a href="">お問い合わせフォーム</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+<?php get_footer();?>
