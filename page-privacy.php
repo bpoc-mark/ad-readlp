858887aab852b950d392582ade3004db88b4f351
bpoc-mark <markariel.maata@gmail.com>
2021-12-25 23:09:45 +0800

    added privacy page pc and sp view

---
+++ page-privacy.php
@@ -0,0 +1,71 @@
+<?php 
+
+/* Template Name: Privacy Page */
+
+get_header();?>
+<div class="page_privacy">
+    <section class="sect-1">
+        <div class="l-wrap">
+            <ul class="privacy">
+                <li class="privacy--item">
+                    <h3 class="privacy--item__ttl">基本理念</h3>
+                    <p class="privacy--item__desc">株式会社リード（以下「当社」）は、以下のとおり個人情報保護方針を定め、個人情報保護の仕組みを構築し、全従業員に個人情報保護の重要性の認識と取組みを徹底させることにより、個人情報の保護を推進致します。</p>
+                </li>
+                <li class="privacy--item">
+                    <h3 class="privacy--item__ttl">個人情報の管理</h3>
+                    <p class="privacy--item__desc">当社は、お客さまの個人情報を正確かつ最新の状態に保ち、個人情報への不正アクセス・紛失・破損・改ざん・漏洩などを防止するため、セキュリティシステムの維持・管理体制の整備・社員教育の徹底等の必要な措置を講じ、安全対策を実施し個人情報の厳重な管理を行ないます。</p>
+                </li>
+                <li class="privacy--item">
+                    <h3 class="privacy--item__ttl">個人情報の利用目的</h3>
+                    <p class="privacy--item__desc">お客さまからお預かりした個人情報は、当社からのご連絡や業務のご案内やご質問に対する回答として、電子メールや資料のご送付に利用いたします。</p>
+                </li>
+                <li class="privacy--item">
+                    <h3 class="privacy--item__ttl">個人情報の第三者への開示・提供の禁止</h3>
+                    <p class="privacy--item__desc">当社は、お客さまよりお預かりした個人情報を適切に管理し、次のいずれかに該当する場合を除き、個人情報を第三者に開示いたしません。</p>
+                    <div class="privacy--item__highlight">
+                        <p>お客さまの同意がある場合</p>
+                        <p>お客さまが希望されるサービスを行なうために当社が業務を委託する業者に対して開示する場合</p>
+                        <p>法令に基づき開示することが必要である場合</p>
+                    </div>
+                </li>
+                <li class="privacy--item">
+                    <h3 class="privacy--item__ttl">個人情報の安全対策</h3>
+                    <p class="privacy--item__desc">当社は、個人情報の正確性及び安全性確保のために、セキュリティに万全の対策を講じています。</p>
+                </li>
+                <li class="privacy--item">
+                    <h3 class="privacy--item__ttl">ご本人の照会</h3>
+                    <p class="privacy--item__desc">お客さまがご本人の個人情報の照会・修正・削除などをご希望される場合には、ご本人であることを確認の上、対応させていただきます。</p>
+                </li>
+                <li class="privacy--item">
+                    <h3 class="privacy--item__ttl">法令、規範の遵守と見直し</h3>
+                    <p class="privacy--item__desc">当社は、保有する個人情報に関して適用される日本の法令、その他規範を遵守するとともに、本ポリシーの内容を適宜見直し、その改善に努めます。</p>
+                </li>
+            </ul>
+        </div>
+    </section>
+    <section class="c-stamp">
+        <div class="l-wrap">
+            <div class="c-stamp--wrap">
+                <h3 class="c-stamp--wrap__ttl">Contact</h3>
+                <p class="c-stamp--wrap__desc">
+                ご依頼に関するお問い合せは下記までご連絡ください。<br>
+                <br>
+                <span>株式会社リード</span><br>
+                〒150-0002 東京都渋谷区渋谷1-11-1 COI西青山ビル4F
+                </p>
+                <div class="c-stamp--wrap__row">
+                    <div class="c-stamp--wrap__row--col">
+                        <a href="tel:03-6418-2475">TEL : 03-6418-2475</a>
+                    </div>
+                    <div class="c-stamp--wrap__row--col">
+                        <span>FAX : 03-6418-2476</span>
+                    </div>
+                    <div class="c-stamp--wrap__row--col">
+                        <a href="">お問い合わせフォーム</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+<?php get_footer();?>
